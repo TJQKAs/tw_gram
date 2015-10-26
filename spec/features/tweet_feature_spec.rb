a0648ea14a5aa6ca80cab79a83847ed68084e8bd
MoeSadoon <msadoon590@gmail.com>
2015-10-26 17:53:22 +0000

    wrote second test and added tweet model

--- spec/features/tweet_feature_spec.rb
+++ spec/features/tweet_feature_spec.rb
@@ -11,4 +11,18 @@ feature 'tweets' do
       expect(page).to have_link 'Add a tweet'
     end
   end
+
+
+
+  context 'tweets have been added' do
+    before { Tweet.create description: 'My first tweet' }
+
+    scenario 'display tweets' do
+      visit '/tweets'
+      expect(page).to have_content 'My first tweet'
+      expect(page).not_to have_content 'You have no tweets yet'
+    end
+  end
+
+
 end
